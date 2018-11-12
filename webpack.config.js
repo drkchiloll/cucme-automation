f3030ec2fafed070487355e2b13ec332c7855b12
drkchiloll <samwomack_@me.com>
2018-11-12 12:44:40 -0600

    update formatting

--- webpack.config.js
+++ webpack.config.js
@@ -1,71 +1,62 @@
 const path = require('path'),
-	fs = require('fs'),
-	webpack = require('webpack'),
-	merge = require('webpack-merge'),
-	UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-
-const { NODE_ENV } = process.env;
-let nodeModules = {};
-
-fs.readdirSync('node_modules')
-	.filter(function (x) {
-		return ['.bin'].indexOf(x) === -1 && x === 'java';
-	})
-	.forEach(function (mod) {
-		nodeModules[mod] = 'commonjs ' + mod;
-	});
+  fs = require('fs'),
+  webpack = require('webpack'),
+  merge = require('webpack-merge'),
+  UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = (env, options) => {
-	let dev = {
-		entry: {
-			main: path.join(__dirname, 'src/electron-main/main.ts'),
-			app: path.join(__dirname, 'src/app/main.tsx')
-		},
-		output: {
-			path: path.join(__dirname, 'dist'),
-			filename: '[name].bundle.js'
-		},
-		target: 'electron-main',
-		node: {
-			__dirname: false,
-			console: true,
-			fs: 'empty',
-			net: 'empty',
-			tls: 'empty'
-		},
-		devtool: 'source-map',
-		resolve: {
-			extensions: ['.ts', '.tsx', '.js']
-		},
-		module: {
-			rules: [{
-				test: /\.tsx?$/,
-				loader: "awesome-typescript-loader",
-				options: {
-					transpileOnly: false
-				}
-			}, {
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader']
-			}, {
-				test: /.woff$|.woff2$|.ttf$|.eot$|.svg$/,
-				loader: 'url-loader'
-			}]
-		},
-		externals: nodeModules
-	};
+  let dev = {
+    entry: {
+      main: path.join(__dirname, 'src/electron-main/main.ts'),
+      app: path.join(__dirname, 'src/app/main.tsx')
+    },
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: '[name].bundle.js'
+    },
+    target: 'electron-main',
+    node: {
+      __dirname: false,
+      console: true,
+      fs: 'empty',
+      net: 'empty',
+      tls: 'empty'
+    },
+    devtool: 'source-map',
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js']
+    },
+    module: {
+      rules: [{
+        test: /\.tsx?$/,
+        loader: "awesome-typescript-loader",
+        options: {
+          transpileOnly: false
+        }
+      }, {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }, {
+        test: /.woff$|.woff2$|.ttf$|.eot$|.svg$/,
+        loader: 'url-loader'
+      }, {
+        test: /\.node$/,
+        use: 'node-loader'
+      }]
+    }
+  };
 
-	let prod = {
-		plugins: [
-			new webpack.LoaderOptionsPlugin({
-				minimize: true,
-				compress: {
-					warnings: false,
-					drop_console: false
-				}
-			})
-		]
-	};
-	if (options.mode === 'production') return merge(dev, prod)
-	else return dev;
+  let prod = {
+    plugins: [
+      new webpack.LoaderOptionsPlugin({
+        minimize: true,
+        compress: {
+          warnings: false,
+          drop_console: false
+        }
+      })
+    ]
+  };
+  if (options.mode === 'production') return merge(dev, prod)
+  else return dev;
 };
