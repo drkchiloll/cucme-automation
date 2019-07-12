de7f823ef2d3f6df6f492a7d7a82c587649b5945
drkchiloll <samwomack_@me.com>
2019-07-12 14:13:31 -0500

    cleanup ...

--- src/app/lib/api.ts
+++ src/app/lib/api.ts
@@ -1,6 +1,6 @@
 /* API */
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-import { Client, ClientChannel, ConnectConfig } from 'ssh2';
+import { Client } from 'ssh2';
 import * as xpath from 'xpath';
 import { DOMParser as dom } from 'xmldom';
 import axios, { AxiosInstance } from 'axios';
@@ -8,7 +8,6 @@ import * as Promise from 'bluebird';
 import { cmeXmlFactory } from './cme-xml';
 import { sshFactory } from './cme-ssh';
 import * as csv from 'csvtojson';
-import { join } from 'path';
 const cmeService = cmeXmlFactory();
 
 const hosts = [{
@@ -146,92 +145,6 @@ export class Api {
   }
 }
 
-const cfg: any = {
-  voiceGlobal: {
-    codecs: [{
-      name: '1',
-      preferences: [
-        'g722-64', 'g711ulaw'
-      ]
-    }],
-    voip: [
-      'no ip address trusted authenticate',
-      'allow connections sip to sip',
-      'fax protocol t38 version 0 ls-redundancy 0 hs-redundancy 0 fallback pass-through g711ulaw',
-      'sip',
-      'registrar server expires max 600 min 60'
-    ],
-    presence: [{
-      subsystem: 'sip-ua',
-      cmds: [
-        'sip-ua',
-        'prsence-enable'
-      ]
-    }, {
-      subsystem: 'presence',
-      cmds: [
-        'presence',
-        'max-subscriptions 500',
-        'presence call-list'
-      ]
-    }]
-  },
-  templates: [{
-    tag: 1,
-    cmds: [
-      'button-layout 1 line',
-      'button-layout 2 blf-speed-dial',
-      'softkeys ringIn Answer iDivert DND',
-      'softkeys seized Redial Endcall Pickup Gpickup Cfwdall Meetme',
-      'conference drop-mode local',
-      'transfer max-length 12'
-    ]
-  }, {
-    tag: 2,
-    cmds: [
-      'button-layout 1-2 line',
-      'button-layout 3-7 blf-speed-dial',
-      'softkeys ringIn Answer iDivert DND',
-      'softkeys seized Redial Endcall Pickup Gpickup Cfwdall Meetme',
-      'conference drop-mode local',
-      'transfer max-length 12'
-    ]
-  }],
-  cor: {
-    custom: [
-      { name: 'emergency' },
-      { name: 'local' },
-      { name: 'longdistance' },
-      { name: 'international' },
-      { name: 'paging-pres' }
-    ],
-    lists: [{
-      name: 'lobby',
-      members: [
-        'emergency', 'local'
-      ]
-    }, {
-      name: 'faculty',
-      members: [
-        'emergency', 'local', 'longdistance'
-      ]
-    }, {
-      name: 'managers',
-      members: [
-        'emergency', 'local', 'longdistance', 'international'
-      ]
-    }],
-    generated() {
-      return this.custom.map((c: any) => {
-        return {
-          name: c.name,
-          member: c.name
-        }
-      })
-    }
-  }
-}
-
 interface SipDn {
   number: number;
   name: string;
@@ -259,18 +172,4 @@ interface SipDevice {
   username: string;
   password: string;
   vad: boolean;
-}
-
-const deviceTemplate = {
-  /**
-   * voice register template  2
-    button-layout 1-2 line
-    button-layout 3 blf-speed-dial
-    button-layout 4-6 feature-button
-    softkeys ringIn  Answer iDivert DND
-    softkeys seized  Redial Endcall Meetme Pickup Gpickup Cfwdall
-    conference drop-mode local
-    transfer max-length 12
-    !
-   */
 }
