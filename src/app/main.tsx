5573cf458d8f740fcfb5aa6c10b914b2122a457b
drkchiloll <samwomack_@me.com>
2019-07-20 09:37:52 -0500

    make sure the initial account is loaded into memory

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -2,4 +2,11 @@ import './vendor';
 import * as React from 'react'
 import * as ReactDom from 'react-dom'
 import { App } from './container';
+let accounts = JSON.parse(localStorage.getItem('accounts'));
+if(!accounts) {
+  localStorage.setItem('accounts', JSON.stringify([{
+    id: 1,
+    password: 'C1sco12345'
+  }]));
+}
 ReactDom.render(<App />, document.getElementById('app'));
